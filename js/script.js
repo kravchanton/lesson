c9d17fd0f4af9b33320d89d52ad8a16b48e28804
Anton <Kravch-anton@Mail.ru>
2021-03-19 19:05:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* 
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+
+
+
+var name = 'Ivan';
+
+console.log(name); */
+
+const numberOfFilms = +prompt('Сколько фильмов выуже просмотрели?', '');
